4f3aa14f9d466e615fc419fc719fa56fa7ea94ac
kms908 <funnycoding91@gmail.com>
2020-11-06 15:55:25 +0900

    first commit zuul, eureka

---
+++ eureka/src/test/java/com/github/kms908/springcloudmsa/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kms908.springcloudmsa.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ zuul/src/test/java/com/github/kms908/springcloudmsa/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kms908.springcloudmsa.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
